27c5ed5b0896d36a0412035a8643a37a8de3c2bc
Renees009 <reneesjenisha@gmail.com>
2026-05-26 11:50:57 +0530

    MYSQL DB setup

--- Server/backend/settings.py
+++ Server/backend/settings.py
@@ -78,10 +78,15 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'lms',
+        'USER': 'root',
+        'PASSWORD': 'Pilot@009',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
